8c0e66aa6f421fd9ccee173dde2efa07dda86a83
André Miranda <aegis.andre@gmail.com>
2024-07-24 10:52:25 -0300

    build: add cors and swagger

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia"
+import { cors } from "@elysiajs/cors"
+import { swagger } from "@elysiajs/swagger"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000)
+const app = new Elysia()
+  .use(
+    cors({
+      origin: "*",
+    })
+  )
+  .use(swagger())
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      default:
+        console.log(error)
+        set.status = 500
+        return "INTERNAL_SERVER_ERROR"
+    }
+  })
+  .listen(3000)
 
 console.log(
   `Equivalent API is running at ${app.server?.hostname}:${app.server?.port}`
