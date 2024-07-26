74cbfe5fd171c983574e88af237f9e7c5cad24a3
André Miranda <aegis.andre@gmail.com>
2024-07-25 22:45:36 -0300

    fix: incorrect config and relations schema

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -4,6 +4,8 @@ import * as schema from "./schema"
 import * as relations from "./relations"
 
 const sqlite = new Database("./drizzle/taco.db")
-export const db = drizzle(sqlite, { schema: { schema, relations } })
+export const db = drizzle(sqlite, {
+  schema: { ...schema, ...relations },
+})
 
 export type SqliteDb = typeof db
--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,37 +1,46 @@
-import { relations } from "drizzle-orm/relations";
-import { categories, foods, amino_acids, fatty_acids, nutrients } from "./schema";
+import { relations } from "drizzle-orm/relations"
+import {
+  categories,
+  foods,
+  amino_acids,
+  fatty_acids,
+  nutrients,
+} from "./schema"
 
-export const foodsRelations = relations(foods, ({one, many}) => ({
-	category: one(categories, {
-		fields: [foods.categoryId],
-		references: [categories.id]
-	}),
-	amino_acids: many(amino_acids),
-	fatty_acids: many(fatty_acids),
-	nutrients: many(nutrients),
-}));
+export const foodsRelations = relations(foods, ({ one, many }) => ({
+  category: one(categories, {
+    fields: [foods.categoryId],
+    references: [categories.id],
+  }),
+  amino_acids: many(amino_acids),
+  fatty_acids: many(fatty_acids),
+  nutrients: one(nutrients, {
+    fields: [foods.id],
+    references: [nutrients.foodId],
+  }),
+}))
 
-export const categoriesRelations = relations(categories, ({many}) => ({
-	foods: many(foods),
-}));
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  foods: many(foods),
+}))
 
-export const amino_acidsRelations = relations(amino_acids, ({one}) => ({
-	food: one(foods, {
-		fields: [amino_acids.foodId],
-		references: [foods.id]
-	}),
-}));
+export const amino_acidsRelations = relations(amino_acids, ({ one }) => ({
+  food: one(foods, {
+    fields: [amino_acids.foodId],
+    references: [foods.id],
+  }),
+}))
 
-export const fatty_acidsRelations = relations(fatty_acids, ({one}) => ({
-	food: one(foods, {
-		fields: [fatty_acids.foodId],
-		references: [foods.id]
-	}),
-}));
+export const fatty_acidsRelations = relations(fatty_acids, ({ one }) => ({
+  food: one(foods, {
+    fields: [fatty_acids.foodId],
+    references: [foods.id],
+  }),
+}))
 
-export const nutrientsRelations = relations(nutrients, ({one}) => ({
-	food: one(foods, {
-		fields: [nutrients.foodId],
-		references: [foods.id]
-	}),
-}));
+export const nutrientsRelations = relations(nutrients, ({ one }) => ({
+  food: one(foods, {
+    fields: [nutrients.foodId],
+    references: [foods.id],
+  }),
+}))
